c8c0d7f3ec84d28d3279eca6d19544e4c3b46db9
omtinez <omtinez@gmail.com>
2017-11-29 13:26:36 -0800

    Final conversion

--- midichlorians.py
+++ midichlorians.py
@@ -3,19 +3,18 @@
 
 # In[12]:
 
-get_ipython().magic('matplotlib inline')
-
 import re
 import os
+import glob
 import datetime
 
 import tqdm
 import PIL.Image
 import PIL.ExifTags
+import exifread
 
 import numpy as np
 import pandas as pd
-import seaborn as sns
 
 
 # In[20]:
@@ -27,7 +26,7 @@ class VideoEncoder:
     def __init__(self, ffmpeg_path='ffmpeg'):
         self.ffmpeg = ffmpeg_path
         self.proc = None
-        
+
     def start(self, outfile, framerate=1, vcodec='mjpeg'):
         if self.proc:
             raise RuntimeError('Calling "start()" before process pipe was closed.')
@@ -40,14 +39,14 @@ class VideoEncoder:
                 '-i', 'pipe:', 
                 '-vcodec', 'libxvid',
                 re.sub(r'[^\d\w\.]', '_', outfile))
-        
+
         self.proc = subprocess.Popen(cmd, stdin=subprocess.PIPE)
-        
+
     def encode(self, framelist):
         ''' `framelist` must be an iterable of raw bytes '''
         for frame in framelist:
             self.proc.stdin.write(frame)
-            
+ 
     def close(self):
         self.proc.stdin.close()
         self.proc = None
@@ -58,30 +57,39 @@ class VideoEncoder:
 # Build a map of filename -> timestamp, camera, resolution
 TS_FORMAT = '%Y:%m:%d %H:%M:%S'
 ts_map = {}
-for fname in [f for f in os.listdir('session') if os.path.isfile(os.path.join('session', f))]:
-    img = PIL.Image.open(os.path.join('session', fname))
-    exif = {
-        PIL.ExifTags.TAGS[k]: v
-        for k, v in img._getexif().items()
-        if k in PIL.ExifTags.TAGS
-    }
-    timestamp = datetime.datetime.strptime(exif.get('DateTime'), TS_FORMAT)
+for fname in glob.iglob('photos/**/*.*', recursive=True):
+    img = PIL.Image.open(fname)
+    exif = None
+    with open(fname, 'rb') as fh:
+        exif = {k: str(v) for k, v in exifread.process_file(fh).items()}
+    #print(exif.get('Image DateTime'))
+ 
+    #exif = {
+    #    PIL.ExifTags.TAGS[k]: v
+    #    for k, v in img._getexif().items()
+    #    if k in PIL.ExifTags.TAGS
+    #}
+    try:
+        timestamp = datetime.datetime.strptime(str(exif.get('Image DateTime')), TS_FORMAT)
+    except:
+        print('Error processing EXIF for image %s' % fname)
+        continue
     ts_map[fname] = (
         timestamp,
-        exif.get('Model').replace('\x00', ''),
-        '%dx%d' % (exif.get('ExifImageHeight'), exif.get('ExifImageWidth')))
+        str(exif.get('Image Model')).replace('\x00', ''),
+        '%dx%d' % (int(exif.get('Image YResolution')), int(exif.get('Image XResolution'))))
 
 
 # In[4]:
 
 # Separate data based on camera type
-df = pd.DataFrame.from_dict(ts_map, orient='index').reset_index()    .rename(columns={'index': 'filename', 0: 'timestamp', 1: 'camera', 2: 'resolution'}).sort_values('timestamp')
+df = pd.DataFrame.from_dict(ts_map, orient='index').reset_index().rename(columns={'index': 'filename', 0: 'timestamp', 1: 'camera', 2: 'resolution'}).sort_values('timestamp')
 cameras = df['camera'].unique().tolist()
 
 df_ = {}
 for camera in cameras:
     df_[camera] = df[df['camera'] == camera]
-    
+
 print('Cameras:', cameras)
 df_[cameras[0]].head()
 
@@ -91,10 +99,10 @@ df_[cameras[0]].head()
 enc = VideoEncoder()
 for camera in cameras:
     df = df_[camera]
-    
+
     # Compute the difference between frames
     df['timediff'] = df['timestamp'].diff().astype('timedelta64[s]')
-    
+
     # Assign values to contiguous frames
     tmp = df['timediff'] > 120
     df['framegroup'] = tmp.astype(int).cumsum()
@@ -102,7 +110,7 @@ for camera in cameras:
     # Filter out frames with bogus resolution
     df['filter'] = 1
     df['area'] = df['resolution'].apply(lambda x: int(x.split('x')[0]) * int(x.split('x')[1]))
-    for group in df['framegroup'].unique():
+    for group in tqdm.tqdm(df['framegroup'].unique()):
         ix = df['framegroup'] == group
         median = df.loc[ix, 'area'].median()
         df.loc[ix, 'filter'] = (df.loc[ix, 'area'] == median).astype(int)
@@ -111,23 +119,23 @@ for camera in cameras:
         frames = df.loc[ix]
         if len(frames) < 10: continue
 
+        # Compute start and end time based on timestamp of frames
         mintime = frames['timestamp'].min()
         maxtime = frames['timestamp'].max()
 
+        # Compute the framerate necessary
         avediff = frames['timediff'].mean()
         framerate = 8.0 * 60.0 / avediff
+
+        if len(frames) < 10: continue
 
         print('Time span: %s - %s (%s)' % (mintime, maxtime, (maxtime - mintime)))
         print('Framerate: %f (~%f)' % (framerate, avediff))
         
-        frames = df[df['filter'] & (df['framegroup'] == group)]
-        if len(frames) < 10: continue
         framerate = 8.0 * 60.0 / frames['timediff'].mean()
         enc.start('%s_%s_seastars.mp4' % (camera, mintime), framerate=framerate)
         for fname in tqdm.tqdm(frames['filename'][:]):
-            img = open(os.path.join('session', fname), 'rb')
+            img = open(fname, 'rb')
             enc.encode([img.read()])
         enc.close()
-
-df[cameras[0]].head()
 
